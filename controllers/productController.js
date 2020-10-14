4ea09ffbc994f131348bed7bb638eee303491b36
Sean Doyle <sdoylelambda@gmail.com>
2020-10-14 14:28:50 -0400

    Controllers and models for add, edit, and delete products.

--- controllers/productController.js
+++ controllers/productController.js
@@ -10,4 +10,26 @@ router.get('/', (req, res) => {
     .catch((err) => res.status(500).json({ err: err.message }))
 })
 
+// add
+router.post('/', (req, res) => {
+  const newProduct = res.body
+  ProductModel.add(newProduct)
+    .then((product) => res.status(200).json(product))
+    .catch((err) => res.status(500).json({ err: err.message }))
+})
+
+// find by.... name, id? price?
+// router.get('/')
+
+router.put('/', (req, res) => {
+  ProductModel.editProduct
+})
+
+router.delete('/:id', (req, res) => {
+  const id = res.header
+  ProductModel.deleteProduct(id)
+    .then((del = res.status(200).json({ msg: 'Product deleted' })))
+    .catch((err = res.status(500).json({ err: err.message })))
+})
+
 module.exports = router
--- models/productsModel.js
+++ models/productsModel.js
@@ -2,8 +2,23 @@ const db = require('../data/db-config')
 
 module.exports = {
   find,
+  addProduct,
+  deleteProduct,
+  editProduct,
 }
 
 function find() {
   return db('products')
+}
+
+function addProduct(newProduct) {
+  return db('products').insert(newProduct)
+}
+
+function editProduct() {
+  return db('products') // TBC
+}
+
+function deleteProduct(id) {
+  return db('products').delete(id) // where id=id
 }
