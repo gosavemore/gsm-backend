973a99624d83e434830560f1dc99bdf78950da45
Sean Doyle <sdoylelambda@gmail.com>
2020-10-15 15:30:49 -0400

    Post for login with verifies with hashed password.

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -17,18 +17,7 @@ router.get('/:username', (req, res) => {
   console.log(username)
   AdminModel.findUser({ username }).then((userData) => {
     const user = userData[0]
-    res.status(200).json({
-      username: user.username,
-      firstName: user.firstName,
-      lastName: user.lastName,
-      password: user.password,
-      houseNumber: user.houseNumber,
-      streetName: user.streetName,
-      city: user.city,
-      zip: user.zip,
-      state: user.state,
-      country: user.country,
-    })
+    res.status(200).json({ user })
   })
 })
 
--- controllers/authController.js
+++ controllers/authController.js
@@ -1,11 +1,28 @@
 const express = require('express')
 const router = express.Router()
 const adminModel = require('../models/adminModel.js')
+
 const bcrypt = require('bcrypt')
 
 // login
-router.get('/login', (req, res) => {
-  //
+router.post('/login', async (req, res) => {
+  try {
+    //
+    let { username, password } = req.body
+    let user = await adminModel.findUser({ username })
+    console.log('user', user)
+    console.log('user[0]', user[0])
+    console.log('password', password)
+    if (user.length === 0) {
+      return res.status(404).json({ message: 'Username not found!' })
+    } else {
+      let passwordVerified = await bcrypt.compare(password, user[0].password)
+      console.log('the password verified', passwordVerified)
+    }
+  } catch (err) {
+    console.log('post error', err)
+    // there's connection error
+  }
 })
 
 router.post('/register', async (req, res) => {
@@ -24,7 +41,9 @@ router.post('/register', async (req, res) => {
         res.status(200).json(user)
       })
       .catch((err) => console.log(err))
-  } catch (err) {}
+  } catch (err) {
+    console.log('post error', err)
+  }
 })
 
 module.exports = router
