8bb232aaf5a0cf201ed2eea41cd8fadac72b0373
denmercs <denmercs@gmail.com>
2020-10-20 15:56:42 -0500

    fixing bugs on knexfile.js production - added migrations and seed directory

--- knexfile.js
+++ knexfile.js
@@ -2,42 +2,42 @@
 
 module.exports = {
   development: {
-    client: 'sqlite3',
+    client: "sqlite3",
     connection: {
-      filename: './dev.sqlite3',
+      filename: "./dev.sqlite3",
     },
     migrations: {
-      directory: './data/migrations',
+      directory: "./data/migrations",
     },
     seeds: {
-      directory: './data/seeds',
+      directory: "./data/seeds",
     },
     // not sure if needed
     pool: {
       afterCreate: (conn, done) => {
-        conn.run('PRAGMA foreign_keys = ON', done)
+        conn.run("PRAGMA foreign_keys = ON", done);
       },
     },
   },
 
   staging: {
-    client: 'postgresql',
+    client: "postgresql",
     connection: {
-      database: 'my_db',
-      user: 'username',
-      password: 'password',
+      database: "my_db",
+      user: "username",
+      password: "password",
     },
     pool: {
       min: 2,
       max: 10,
     },
     migrations: {
-      tableName: 'knex_migrations',
+      tableName: "knex_migrations",
     },
   },
 
   production: {
-    client: 'postgresql',
+    client: "postgresql",
     connection: process.env.DATABASE_URL || {
       database: process.env.DB,
       user: process.env.DB_USER,
@@ -48,8 +48,11 @@ module.exports = {
       max: 10,
     },
     migrations: {
-      tableName: 'knex_migrations',
+      directory: "./data/migrations",
+    },
+    seeds: {
+      directory: "./data/seeds",
     },
     useNullAsDefault: true,
   },
-}
+};
