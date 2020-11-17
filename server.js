001f7359a09cc5ed31d90a69ffff709e466d9019
Sean Doyle <sdoylelambda@gmail.com>
2020-11-17 15:32:17 -0500

    Stripe routes payments intents created.

--- server.js
+++ server.js
@@ -1,25 +1,27 @@
-const express = require("express");
-const cors = require("cors");
-const helmet = require("helmet");
-const productsRouter = require("./controllers/productController");
-const adminRouter = require("./controllers/adminController");
-const authRouter = require("./controllers/authController.js");
-const cartRouter = require("./controllers/cartController.js");
-const orderRouter = require("./controllers/orderController");
-const server = express();
+const express = require('express')
+const cors = require('cors')
+const helmet = require('helmet')
+const productsRouter = require('./controllers/productController')
+const adminRouter = require('./controllers/adminController')
+const authRouter = require('./controllers/authController.js')
+const cartRouter = require('./controllers/cartController.js')
+const orderRouter = require('./controllers/orderController')
+const stripeRouter = require('./controllers/stripeController')
+const server = express()
 
-server.use(cors());
-server.use(helmet());
-server.use(express.json());
+server.use(cors())
+server.use(helmet())
+server.use(express.json())
 
 // Routes
-server.use("/products", productsRouter);
-server.use("/admin", adminRouter); // RUD function admin previledge
-server.use("/auth", authRouter); // registration and login
-server.use("/cart", cartRouter); // orders
-server.use("/orders", orderRouter);
-server.get("/", (req, res) => {
-  res.json({ api: "Backend is running" });
-});
+server.use('/products', productsRouter)
+server.use('/admin', adminRouter) // RUD function admin previledge
+server.use('/auth', authRouter) // registration and login
+server.use('/cart', cartRouter) // orders
+server.use('/orders', orderRouter)
+server.use('/checkout', stripeRouter)
+server.get('/', (req, res) => {
+  res.json({ api: 'Backend is running' })
+})
 
-module.exports = server;
+module.exports = server
