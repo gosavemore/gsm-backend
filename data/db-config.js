6641c5f82ba33fea737a433474f8449625132ad9
Sean Doyle <sdoylelambda@gmail.com>
2020-12-14 12:56:20 -0500

    Tests for auth routes.

--- data/db-config.js
+++ data/db-config.js
@@ -1,6 +1,6 @@
-const knex = require("knex");
-const knexFile = require("../knexfile");
+const knex = require('knex')
+const knexFile = require('../knexfile')
 
-const environment = process.env.DB_ENV || "development";
+const environment = process.env.DB_ENV || 'development'
 
 module.exports = knex(knexFile[environment])
